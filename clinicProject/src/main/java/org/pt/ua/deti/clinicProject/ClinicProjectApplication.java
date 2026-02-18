b027ab21334cfb9063d16f0d6a44cd048d0d9273
Marshmallow1119 <tomasbras@ua.pt>
2026-02-18 09:37:48 +0000

    project setup

---
+++ clinicProject/src/main/java/org/pt/ua/deti/clinicProject/ClinicProjectApplication.java
@@ -0,0 +1,13 @@
+package org.pt.ua.deti.clinicProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicProjectApplication.class, args);
+	}
+
+}
---
+++ clinicProject/src/test/java/org/pt/ua/deti/clinicProject/ClinicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pt.ua.deti.clinicProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
